10396316f9b435ae07fc0f5ed30dccf207cf55c0
Piotr Krasny <krasny.piotr@gmail.com>
2021-04-14 09:35:11 +0200

    Initial

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,77 @@
+import dotenv from 'dotenv';
+import projectConfig from './src/projectConfig';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: projectConfig.title,
+    description: projectConfig.description,
+    siteUrl: projectConfig.siteUrl,
+    language: projectConfig.language,
+  },
+
+  flags: { DEV_SSR: true },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `10e4smak`,
+        token: process.env.SANITY_TOKEN,
+        dataset: `production`,
+        watchMode: true,
+        apiVersion: '2021-04-14',
+        graphqlTag: 'default',
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/assets/images/`,
+        name: `images`,
+      },
+      __key: `images`,
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 630,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
+      },
+    },
+    `gatsby-remark-copy-linked-files`,
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sitemap',
+    'gatsby-plugin-offline',
+    // {
+    //   resolve: "gatsby-plugin-manifest",
+    //   options: {
+    //     icon: "src/images/icon.png",
+    //   },
+    // },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: {
+        trackingId: projectConfig.trackingGoogleId,
+      },
+    },
+  ],
+};
---
+++ gatsby/src/projectConfig.js
@@ -0,0 +1,10 @@
+const projectConfig = {
+  title: 'Gatsby boilerplate',
+  description: 'Gatsby starter site',
+  siteUrl: 'https://pkgatsbystarter.netlify.com',
+  language: 'en',
+  pagesAmountInSet: 2,
+  trackingGoogleId: 'sdsdsdsds',
+};
+
+export default projectConfig;
